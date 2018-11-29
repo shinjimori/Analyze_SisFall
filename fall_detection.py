ce581b842ad5f8aaa730659a2426756d57d0e54d
s.omori <s.omori@d-clue.com>
2018-11-29 10:22:11 +0900

    [add] AnalyzeSisFallクラスを実装
    - データセット読み込みまでを実装
    - データセットのファイルリストを取得し、そこから各ファイルのデータにアクセスする
    - dataframeに列を追加するなどの処理を追加予定

--- fall_detection.py
+++ fall_detection.py
@@ -5,8 +5,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.tests.io.test_parquet import df_cross_compat
 
+
+class AnalyzeSisFall:
+
+    file_list = ""
+    activity_list = []
+
+    def __init__(self, path):
+        self.path = path
+        self.file_list = glob.glob(self.path)
+        self.__create_activity_list()
+        self.df = self.__convert_list_to_dataframe()
+        # print(self.df)
+
+# Private
+
+    def __load_data(self, file_name):
+        df = pd.read_csv(file_name, usecols=[1, 2, 3], names={'ax':[1], 'ay':[2], 'az':[3]})
+        return df
+
+    def __create_activity_list(self):
+        for i, v in enumerate(self.file_list):
+            label, ext = os.path.splitext(os.path.basename(v))
+            activity, subject, trial = label.split("_")
+            self.activity_list.append([v, activity, subject, trial])
+
+    def __convert_list_to_dataframe(self):
+        df = pd.DataFrame(self.activity_list, columns={'path':[1], 'subject':[2], 'activity':[3], 'trial':[4]})
+        return df
+
+    # def __convert_accel(self, data_frame):
+
+# Public
+    def analyze_activities(self):
+        # print(self.df['path'])
+        for i, path in enumerate(self.df['path']):
+            raw_accel = self.__load_data(path)
+            print(raw_accel.describe())
+
+
 if __name__ == '__main__':
-   file_list = glob.glob("C:/Dataset/SisFall_dataset/*/*.txt")
-   print(len(file_list))
-   df_sample = pd.read_csv('C:\Dataset\SisFall_dataset\SA01\D01_SA01_R01.txt', sep=',', names=('Ax_0', 'Ay_0', 'Az_0', 'Gx', 'Gy', 'Gz', 'Ax_1', 'Ay_1', 'Az_1'))
-   print(df_sample)
+    AS = AnalyzeSisFall('C:/Dataset/SisFall_dataset/*/*.txt')
+    # print(AS.df)
+    AS.analyze_activities()
